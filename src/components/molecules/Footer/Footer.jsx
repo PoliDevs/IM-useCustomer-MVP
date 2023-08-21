b5a35653e0b6b88e5635907adaea8b48c628f760
Cesar <cesar.alcoholado@hotmail.com>
2023-08-21 15:53:23 -0300

    Home update mobile design

--- src/components/molecules/Footer/Footer.jsx
+++ src/components/molecules/Footer/Footer.jsx
@@ -1,14 +1,15 @@
 import HugeTitle from "../../atoms/HugeTitle/HugeTitle";
 import SubTitle from "../../atoms/SubTitle/SubTitle";
-import LinkButton from "../../atoms/LinkButton/LinkButton";
 import s from "./Footer.module.scss";
 
 export default function Footer() {
   return (
     <footer className={s.footer}>
+      <div className={s.textContainer}>
       <SubTitle text={"0 productos"} alignment={"left"}/>
       <HugeTitle text={"Total: $0"}/>
-      <LinkButton text={"Ver productos"}/>
+      </div>
+      <button className={s.seeProducts}>Ver productos</button>
     </footer>
   )
 }
