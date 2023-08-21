dc432b6475578594c89140b4818919fef23613cd
Cesar <cesar.alcoholado@hotmail.com>
2023-08-21 14:46:00 -0300

    Added main products slider

--- src/components/atoms/Paragraph/Paragraph.jsx
+++ src/components/atoms/Paragraph/Paragraph.jsx
@@ -1,11 +1,11 @@
 import s from "./Paragraph.module.scss";
 
-export default function Paragraph({text, children, bold, centered, secundary}) {
+export default function Paragraph({text, children, bold, centered, secundary, alignment}) {
   return (
     <p
       className={`${s.paragraph} ${bold ? s.bold : ""} ${
         centered ? s.centered : ""
-      } ${secundary ? s.secundary : ""}`}
+      } ${secundary ? s.secundary : ""} ${alignment === "left" ? s.left : ""}`}
     >
       {text}
       {children}
