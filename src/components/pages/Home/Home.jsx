c5e6fdb105fc8095dd72ca119cc330f66c963733
Cesar <cesar.alcoholado@hotmail.com>
2023-08-23 11:02:01 -0300

    add product feature added

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -4,14 +4,26 @@ import SearchBar from "../../molecules/SearchBar/SearchBar";
 import s from "./Home.module.scss";
 import Products from "../../molecules/Products/Products";
 import Footer from "../../molecules/Footer/Footer";
+import { useEffect, useState } from "react";
 export default function Home() {
+  const [cant, setCant] = useState(localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")) : []);
+
+  const addProduct = async (name, price) => {
+     setCant(prevCant => [...prevCant, { name: name, price: price }]);
+    };
+    
+    useEffect(() => {
+      
+      localStorage.setItem("cart", JSON.stringify(cant));
+  }, [cant]);
+
   return (
     <main className={s.home}>
       <Banner />
       <SearchBar />
-      <Categories/>
-      <Products/>
-      <Footer/>
+      <Categories />
+      <Products addProduct={addProduct} />
+      <Footer cant={cant} setCant={setCant}/>
     </main>
   );
 }
--- src/redux/actions/index.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-import { OPEN_STORE } from "./actionTypes";
-
-export const OpenStore=()=>{
-    
-}
