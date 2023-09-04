62a64a642083be6d8ce85ed7579b4b932d785af4
Cesar <cesar.alcoholado@hotmail.com>
2023-09-04 17:41:47 -0300

    Updated category functionallity

--- src/components/molecules/Icon/Icon.jsx
+++ src/components/molecules/Icon/Icon.jsx
@@ -1,11 +1,11 @@
 /* eslint-disable react/prop-types */
 import s from "./Icon.module.scss";
 
-export default function Icon({icon}) {
+export default function Icon({icon, handleCategory, category}) {
   return (
-    <div className={s.iconContainer}>
-      <img className={s.icon} src={icon.src}/>
+    <div className={`${s.iconContainer} ${(category === icon.category) && s.selected}`} onClick={() => handleCategory(icon.category)}>
+      <img className={s.icon} src={icon.src} />
       <h4 className={s.name}>{icon.name}</h4>
     </div>
-  )
+  );
 }
