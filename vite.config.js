7c7dbc2e192078822b9783a35193de961d20e53c
Cesar <cesar.alcoholado@hotmail.com>
2023-10-14 17:35:01 -0300

    Deleted vite plugin sass

--- vite.config.js
+++ vite.config.js
@@ -2,31 +2,31 @@ import { defineConfig } from 'vite'
 import replace from "@rollup/plugin-replace";
 import svgr from "vite-plugin-svgr";
 import react from '@vitejs/plugin-react'
-import ViteSassPlugin from 'vite-plugin-sass';
+// import ViteSassPlugin from 'vite-plugin-sass';
 import dotenv from "dotenv";
 
 dotenv.config();
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(),ViteSassPlugin()],
+  plugins: [react(), svgr()],
   cors: true, // HabiliCORS en el servidor de desarrollo
-  build: {
-    rollupOptions: {
-      plugins: [
-        // Reemplaza las variables de entorno durante la construcción
-        replace(
-          {
-            "process.env.REACT_APP_CLAVE_SECRETA": JSON.stringify(
-              process.env.VITE_REACT_APP_KEY
-            ),
-          },
-          {
-            "process.env.VITE_MICROSOFT_TRANSLATE_KEY": JSON.stringify(
-              process.env.VITE_MICROSOFT_TRANSLATE_KEY
-            ),
-          }
-        ),
-      ],
-    },
-  },
+  // build: {
+  //   rollupOptions: {
+  //     plugins: [
+  //       // Reemplaza las variables de entorno durante la construcción
+  //       replace(
+  //         {
+  //           "process.env.REACT_APP_CLAVE_SECRETA": JSON.stringify(
+  //             process.env.VITE_REACT_APP_KEY
+  //           ),
+  //         },
+  //         {
+  //           "process.env.VITE_MICROSOFT_TRANSLATE_KEY": JSON.stringify(
+  //             process.env.VITE_MICROSOFT_TRANSLATE_KEY
+  //           ),
+  //         }
+  //       ),
+  //     ],
+  //   },
+  // },
 });
